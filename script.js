40c2a6cd75824e2f6fb8625115002e318fc66c84
Remigiusz Mikolajewicz <r.mikolajewicz@interia.pl>
2022-03-17 20:43:50 +0100

    fixed sliding image animation

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+// const ball = document.querySelector(".travel__cursor");
 
+// document.addEventListener("mousemove", function(event){
+//     ball.style.top = event.pageY + "px";
+//     ball.style.left = event.pageX + "px";
+   
+// })
